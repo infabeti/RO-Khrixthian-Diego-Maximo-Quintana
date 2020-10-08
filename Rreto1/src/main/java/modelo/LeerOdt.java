e3b7e16eb1a1928331da76ecbdc8e7432e30bc2a
khrixthian <cristian.fernandezro@elorrieta-errekamari.com>
2020-10-08 19:17:53 +0200

    ahora si ke escribo en txt y odt sin problemas ya sin los comentarios

--- Rreto1/src/main/java/modelo/LeerOdt.java
+++ Rreto1/src/main/java/modelo/LeerOdt.java
@@ -9,20 +9,6 @@ import org.jopendocument.dom.text.TextDocument;
 public class LeerOdt {
 	public static String LecturadOdt() {
 		String contenidoOdt = "";
-
-		// Obtengo el documento, a partir del Odt
-		//	String Dir = ".//ficheros//trabajadores.odt";
-		//	File archivo = new File(Dir);
-
-	//	try {
-			// Cojo el contenido del documento
-		//		contenidoOdt = TextDocument.createFromFile(archivo).getCharacterContent(true);
-		
-		//	} catch (IOException e) {
-		//		e.printStackTrace();
-			//	}
-		
-		//prueba que funciona
 		try {
             FileReader entrada=new FileReader(".//ficheros//trabajadores.odt");
 
@@ -38,14 +24,10 @@ public class LeerOdt {
 
                 entrada.close();
 
-                System.out.println(contenidoOdt);
-
         } catch (IOException e) {
 
             System.out.println("No se ha encontrado el archivo");
         }
-		// fin prueba
-
 		return contenidoOdt;
 	}
 
