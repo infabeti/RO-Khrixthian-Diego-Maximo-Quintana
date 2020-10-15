e092e84a44335ae574b33bc90a2a6dd60fc56e85
khrixthian <cristian.fernandezro@elorrieta-errekamari.com>
2020-10-15 22:14:58 +0200

    subidas librerias para teses mockito

---
+++ src/test/java/controlador/PRUEBA_ControladorGesEscOdtTest.java
@@ -0,0 +1,20 @@
+package controlador;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import modelo.AgregarAlDocx;
+
+public class PRUEBA_ControladorGesEscOdtTest {
+
+	public static final String aaa = "*";
+	
+	@Test
+	public void name() {
+		AgregarAlDocx x = new AgregarAlDocx();
+		//when(x.get)
+		//https://unpocodejava.com/2012/07/11/un-poco-de-mockito/
+}
+}
